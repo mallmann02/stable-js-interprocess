86289444373aab6e5df104ebe63ad5758e3bd5be
mallmann02 <leomallmann020302@gmail.com>
2023-02-01 08:53:30 -0300

    feat(landing): add filter input field and start creating the globnal states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react';
+import { getDataFromLocalStorage } from './utils/localStorageActions';
+
 function App() {
+  const [ filter, setFilter ] = useState([]);
+  const [ patients, setPatients ] = useState([]);
+
+  useEffect(() => {
+    setPatients(getDataFromLocalStorage);
+  }, []);
+
   return (
     <div className="App">
-      <h1> ACME </h1>
+      <div>
+        <h1> Clínica ACME </h1>
+        <h3> Página de cadastro e pesquisa de pacientes </h3>
+      </div>
+      <label>
+        Filtro
+        <input
+          type="text"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          placeholder="Pesquise pelo nome aqui"
+        />
+      </label>
+      
     </div>
   );
 }
