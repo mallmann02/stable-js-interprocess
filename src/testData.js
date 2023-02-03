515f4b3166ab74dfb6d1f82a593f516d1df5e61d
mallmann02 <leomallmann020302@gmail.com>
2023-02-03 17:14:35 -0300

    feat(test): create test data

---
+++ src/testData.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        name: 'Fulano da Silva',
+        cpf: '74574585612',
+        gender: 'Masculino',
+        bornDate: '2024-02-01',
+        status: 'Ativo',
+        address: 'Rua Ciclano da Silva, 323',
+    },
+    {
+        name: 'Maraisa da Volta',
+        cpf: '82546874123',
+        gender: 'Feminino',
+        bornDate: '2023-02-01',
+        status: 'Ativo',
+        address: 'Rua Maraisa da Volta, 523',
+    },
+    {
+        name: 'Marciana de Marte',
+        cpf: '74585465854',
+        gender: 'Feminino',
+        bornDate: '1997-02-01',
+        status: 'Ativo',
+        address: 'Rua Marciano de Marte, 1523',
+    },
+    {
+        name: 'Jupiteriano de Jupiter',
+        cpf: '47856985125',
+        gender: 'Masculino',
+        bornDate: '2000-02-07',
+        status: 'Inativo',
+        address: 'Rua Jupiteriano Dias, 552',
+    }
+]
