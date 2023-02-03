4d2398ca4b86b9859b556db027295ea9c0abe42e
mallmann02 <leomallmann020302@gmail.com>
2023-02-03 17:09:20 -0300

    feat(test): test user register through the form

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 
 import App from './App';
@@ -8,6 +9,9 @@ import testData from './testData';
 const populateLocalStorage = () => global.localStorage.setItem('patients', JSON.stringify(testData));
 
 const cleanLocalStorage = () => global.localStorage.clear();
+
+const INPUT_SELECTOR = '.main-form__input';
+const SELECT_SELECTOR = '.main-form__select';
 
 describe('1 - Dados do localStorage são resgatados e a tabela é preenchida com estes dados', () => {
     beforeEach(populateLocalStorage);
@@ -39,5 +43,72 @@ describe('1 - Dados do localStorage são resgatados e a tabela é preenchida com
                 expect(status).toHaveProperty('disabled', false);
             }
         }
+    });
+})
+
+describe('2 - É possível cadastrar novos pacientes através do formulário', () => {
+    beforeEach(populateLocalStorage);
+    afterEach(cleanLocalStorage);
+
+    it('Botão para adicionar novo cliente é renderizado e, ao ser clicado, abre o formulário', async () => {
+        await act(async () => {
+            render(<App />);
+        });
+
+        const addNewPatientBtn = screen.getByRole('button', { name: /cadastrar novo paciente/i });
+
+        expect(addNewPatientBtn).toBeInTheDocument;
+
+        fireEvent.click(addNewPatientBtn);
+
+        const form = document.body.getElementsByClassName('patients-register__form');
+        expect(form).toBeInTheDocument;
+    });
+
+    it('O paciente é cadastrado com os dados preenchidos no formulário', async () => {
+        await act(async () => {
+            render(<App />);
+        });
+
+        jest.spyOn(Object.getPrototypeOf(window.localStorage), 'setItem')
+
+        const addNewPatientBtn = screen.getByRole('button', { name: /cadastrar novo paciente/i });
+
+        fireEvent.click(addNewPatientBtn);
+
+        const nameInput = await document.body.querySelector(INPUT_SELECTOR+'.name');
+        const cpfInput = await document.body.querySelector(INPUT_SELECTOR+'.cpf');
+        const genderSelect = await document.body.querySelector(SELECT_SELECTOR+'.gender');
+        const bornDateInput = await document.body.querySelector(INPUT_SELECTOR+'.bornDate');
+        const statusSelect = await document.body.querySelector(SELECT_SELECTOR+'.status');
+        const addressInput = await document.body.querySelector(INPUT_SELECTOR+'.address');
+
+        userEvent.type(nameInput, testData[0].name);
+        userEvent.type(cpfInput, '12345678922'); // Change CPF to avoid "Already registered this CPF" error
+        fireEvent.change(bornDateInput, {target: {value: testData[0].bornDate}})
+        userEvent.selectOptions(genderSelect, testData[0].gender);
+        userEvent.selectOptions(statusSelect, testData[0].status);
+        userEvent.type(addressInput, testData[0].address);
+
+        const submitBtn = document.body.querySelector('.main-form__btn.register');
+
+        fireEvent.click(submitBtn);
+
+        expect(window.localStorage.setItem).toHaveBeenCalled;
+
+        const registeredPatients = JSON.parse(window.localStorage.getItem('patients'));
+
+        const lastRegisteredPatient = registeredPatients[4];
+        delete lastRegisteredPatient.created;
+        delete lastRegisteredPatient.edited;
+
+        expect(lastRegisteredPatient).toStrictEqual({ 
+            name: testData[0].name,
+            gender: testData[0].gender,
+            status: testData[0].status,
+            bornDate: testData[0].bornDate,
+            address: testData[0].address,
+            cpf: '12345678922'
+        });
     });
 })
